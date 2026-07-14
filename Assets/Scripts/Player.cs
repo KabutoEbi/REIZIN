dc2393d8ba12eb9a00251c978bce07384a0c6cf5
KabutoEbi <narenohatehe@gmail.com>
2026-07-15 01:56:41 +0900

    いろいろやった

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class Player : MonoBehaviour {
     [SerializeField] private Transform cameraTransform;
     [SerializeField] private float moveSpeed = 5f;
@@ -10,9 +11,12 @@ public class Player : MonoBehaviour {
     [SerializeField] private float minPitch = -80f;
     [SerializeField] private float maxPitch = 80f;
 
+    private CharacterController characterController;
     private float pitch;
 
     void Start() {
+        characterController = GetComponent<CharacterController>();
+
         if (cameraTransform == null) {
             Camera mainCamera = Camera.main;
             if (mainCamera != null) {
@@ -35,7 +39,7 @@ public class Player : MonoBehaviour {
         float moveZ = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * moveX + transform.forward * moveZ;
-        transform.position += move * moveSpeed * Time.deltaTime;
+        characterController.Move(move * moveSpeed * Time.deltaTime);
 
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
--- Assets/Scripts/TitleManager.cs
+++ Assets/Scripts/TitleManager.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class TitleManager : MonoBehaviour {
-    public void StartGame() {
-        SceneManager.LoadScene("GameScene");
+    public void StartEvent() {
+        SceneManager.LoadScene("EventScene");
     }
 }
